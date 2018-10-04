94d15e5de124932315d23016ef7c8343e0ed67c0
Joshua Byrd <phocks@gmail.com>
2018-10-04 14:03:32 +1000

    No testing allowed

--- src/components/App/AgeChooser/index.js
+++ src/components/App/AgeChooser/index.js
@@ -3,10 +3,14 @@ const styles = require('./styles.scss');
 
 class AgeChooser extends React.Component {
   render() {
+
+console.log(..."hello")
+
+
     return (
       <div className={styles.wrapper}>
         <div className={styles.question}>What age-group are you interested in?</div>
-        <button>All</button>
+        {/* <button>All</button> */}
       </div>
     );
   }
