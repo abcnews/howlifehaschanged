63335fc2abb324ff724cf13e7928416e73870430
Joshua Byrd <phocks@gmail.com>
2018-10-11 15:15:18 +1000

    Attach to node

--- src/components/SlopeChart/index.js
+++ src/components/SlopeChart/index.js
@@ -2,24 +2,25 @@ const React = require("react");
 const styles = require("./styles.scss");
 const d3 = Object.assign({}, require("d3-selection"));
 
-
 class SlopeChart extends React.Component {
   attachChart = () => {
     console.log("Attaching charts...");
-    console.log(this.node)
-    const wrapper = d3.select("." + styles.wrapper);
+    console.log(this.node);
+    const wrapper = d3.select(this.node);
     const svg = wrapper.append("svg");
-  
-    svg.attr("width", 400).attr("height", 400).style("background-color", "crimson");
-  
-  
+
+    svg
+      .attr("width", 400)
+      .attr("height", 400)
+      .style("background-color", "crimson");
+
     console.log(styles.wrapper);
-  }
+  };
   componentDidMount() {
     this.attachChart();
   }
   render() {
-    return <div ref={node => this.node = node} className={styles.wrapper} />;
+    return <div ref={node => (this.node = node)} className={styles.wrapper} />;
   }
 }
 
