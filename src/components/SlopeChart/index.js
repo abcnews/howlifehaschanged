4da9321d888b4b554e5fcad741b546f12c63f7d8
Joshua Byrd <phocks@gmail.com>
2018-10-17 11:49:15 +1000

    Start and end dots

--- src/components/SlopeChart/index.js
+++ src/components/SlopeChart/index.js
@@ -3,7 +3,10 @@ const styles = require("./styles.scss");
 const d3 = Object.assign({}, require("d3-selection"), require("d3-scale"));
 
 const CHART_WIDTH = 350;
-const MARGIN = 10;
+const MARGIN_TOP = 15;
+const MARGIN_RIGHT = 20;
+const MARGIN_BOTTOM = 15;
+const MARGIN_LEFT = 20;
 
 class SlopeChart extends React.Component {
   constructor(props) {
@@ -17,31 +20,57 @@ class SlopeChart extends React.Component {
     const first = this.props.lines[0].first;
     const last = this.props.lines[0].last;
 
+    const min = Math.min(first, last);
+    const max = Math.max(first, last);
+
     const chartHeight = Math.abs(first - last);
 
     const wrapper = d3.select(this.node.current);
     const svg = wrapper.append("svg");
 
+    const scaleHeight = d3
+      .scaleLinear()
+      .domain([min, max])
+      .range([chartHeight - MARGIN_BOTTOM, 0 + MARGIN_TOP]);
+
     const scaleX = d3
       .scaleLinear()
-      .domain([10, 130])
-      .range([0, 960]);
+      .domain([0, CHART_WIDTH])
+      .range([0 + MARGIN_LEFT, CHART_WIDTH - MARGIN_RIGHT]);
+
+    console.log(scaleX(0));
 
     const didIncrease = () => first < last;
 
     svg
       .attr("width", this.props.width || CHART_WIDTH)
       .attr("height", chartHeight)
-      .style("background-color", "rgba(0,0,0,0.1");
+      .style("background-color", "rgba(0, 0, 0, 0.1");
 
-    var line = svg
+    svg
       .append("line")
-      .attr("x1", 0 + MARGIN)
-      .attr("y1", didIncrease() ? chartHeight - MARGIN : 0 + MARGIN)
-      .attr("x2", 350 - MARGIN)
-      .attr("y2", didIncrease() ? 0 + MARGIN : chartHeight - MARGIN)
+      .attr("x1", 0 + MARGIN_LEFT)
+      .attr("y1", scaleHeight(first))
+      .attr("x2", CHART_WIDTH - MARGIN_RIGHT)
+      .attr("y2", scaleHeight(last))
       .attr("stroke-width", 2)
       .attr("stroke", "#FFD70D");
+
+    // Start circle
+    svg
+      .append("circle")
+      .attr("cx", scaleX(0))
+      .attr("cy", scaleHeight(first))
+      .attr("r", 5)
+      .attr("fill", "#FFD70D");
+
+    // End circle
+    svg
+      .append("circle")
+      .attr("cx", scaleX(CHART_WIDTH))
+      .attr("cy", scaleHeight(last))
+      .attr("r", 5)
+      .attr("fill", "#FFD70D");
   };
 
   componentDidMount() {
