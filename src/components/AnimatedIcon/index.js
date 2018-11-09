a30ab48d4f809944479b4b144aa7ca47cf353893
Joshua Byrd <phocks@gmail.com>
2018-11-09 12:02:40 +1000

    Fiddly nudges

--- src/components/AnimatedIcon/index.js
+++ src/components/AnimatedIcon/index.js
@@ -1,15 +1,37 @@
-const React = require('react');
-const styles = require('./styles.scss');
+const React = require("react");
+const styles = require("./styles.scss");
 const Keyshape = require("react-keyshape").Keyshape;
+const d3 = Object.assign({}, require("d3-selection"));
 
 class AnimatedIcon extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.node = React.createRef();
+  }
+
+  componentDidMount() {
+    const svg = this.node.current
+
+    setTimeout(() => {
+      console.log(svg.querySelector(".keyshape-svg svg"));
+    }, 2000)
+    
+  }
+
   render() {
     return (
-      <div className={styles.wrapper}>
+      <div
+        className={styles.wrapper}
+        style={{ marginBottom: "100px" }}
+        ref={this.node}
+      >
         <Keyshape svg={this.props.svg} />
       </div>
     );
   }
+
+  static defaultProps = {};
 }
 
 module.exports = AnimatedIcon;
