0b6381715c01058387312b181a6af5259f090c59
Joshua Byrd <phocks@gmail.com>
2018-11-20 09:47:44 +1000

    Clean ups and stuff

--- src/components/AnimatedIcon/index.js
+++ src/components/AnimatedIcon/index.js
@@ -1,6 +1,5 @@
 const React = require("react");
 const styles = require("./styles.scss");
-const d3 = Object.assign({}, require("d3-selection"));
 
 class AnimatedIcon extends React.Component {
   constructor(props) {
@@ -10,28 +9,19 @@ class AnimatedIcon extends React.Component {
   }
 
   componentDidMount() {
+    // THIS IS NOW HANDLED BY MANUALLY SETTING THE HEIGHT OF SVGS
     // setTimeout(() => {
     //   const svg = d3.select(this.node.current.querySelector("svg"));
     //   // const svgSelection = d3.select(this.node.current.querySelector("svg"));
-
     //   const viewBox = svg.attr("viewBox").split(" ");
-
     //   const vbWidth = viewBox[2];
     //   const vbHeight = viewBox[3];
-
     //   console.log(vbWidth, vbHeight)
-
     //   console.log(svg.node().getBBox());
-
     //   const bbWidth = svg.node().getBBox().width;
-      
-
     //   console.log(bbWidth)
-      
-
     //   const newSvgHeight = bbWidth / vbWidth * vbHeight
     //   console.log(newSvgHeight)
-
     //   svg.attr("height", newSvgHeight / 2);
     // }, 10000);
   }
@@ -45,7 +35,6 @@ class AnimatedIcon extends React.Component {
           paddingRight: this.props.paddingRight + "px",
           paddingTop: this.props.paddingTop + "px",
           paddingBottom: this.props.paddingBottom + "px",
-          marginBottom: this.props.marginBottom + "px",
           width: this.props.width + "px",
           height: this.props.height + "px",
           transform: `translate(${this.props.nudgeX}px, ${this.props.nudgeY}px)`
